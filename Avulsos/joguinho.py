85a5c2fee6f2a764dd8d73369784d5118ddcd5b8
Jefferson Silva <jeferson.aps170@gmail.com>
2026-03-11 15:46:10 -0300

    Jogo pedra, papel e tesoura criado!

---
+++ Avulsos/joguinho.py
@@ -0,0 +1,24 @@
+# Pedra, papel, tesoura!
+import random
+
+opcoes = ['pedra', 'tesoura', 'papel']
+
+while True:
+    print('Pedra, Papel, Tesoura game!\n')
+    player = input('Escolha, Pedra, Papel ou Tesoura: ').lower()
+    npc = random.choice(opcoes)
+    print(f'O computador escolheu: {npc}')
+    if player == npc:
+        print('EMPATE!')
+    elif (player == 'pedra' and npc == 'tesoura') or \
+         (player == 'papel' and npc == 'pedra') or \
+         (player == 'tesoura' and npc == 'papel'):
+        print('Você venceu! 🎇')
+    elif player in opcoes:
+        print('Você perdeu! 🎃')
+    else:
+        print('Opção invalida!')
+    
+    restart = input('Quer jogar novamente? (y/n) > ')
+    if restart != 'y':
+        break
